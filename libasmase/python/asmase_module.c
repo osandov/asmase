d847c839fab0c8258efde7326deba768f8167517
Omar Sandoval <osandov@osandov.com>
2016-12-17 17:29:29 -0800

    Add Python bindings for assembler interface

---
+++ libasmase/python/asmase_module.c
@@ -0,0 +1,39 @@
+#include <Python.h>
+
+#include <libasmase/libasmase.h>
+
+extern PyObject *AssemblerDiagnostic;
+extern PyTypeObject Assembler_type;
+
+static struct PyModuleDef asmasemodule = {
+	PyModuleDef_HEAD_INIT,
+	"asmase",
+	"Interactive assembly language library",
+	-1,
+};
+
+PyMODINIT_FUNC
+PyInit_asmase(void)
+{
+	PyObject *m;
+
+	if (libasmase_init() == -1)
+		return PyErr_SetFromErrno(PyExc_OSError);
+
+	Assembler_type.tp_new = PyType_GenericNew;
+	if (PyType_Ready(&Assembler_type) < 0)
+		return NULL;
+
+	m = PyModule_Create(&asmasemodule);
+	if (m == NULL)
+		return NULL;
+
+	AssemblerDiagnostic = PyErr_NewException("asmase.AssemblerDiagnostic",
+						 NULL, NULL);
+	Py_INCREF(AssemblerDiagnostic);
+	PyModule_AddObject(m, "AssemblerDiagnostic", AssemblerDiagnostic);
+
+	Py_INCREF(&Assembler_type);
+	PyModule_AddObject(m, "Assembler", (PyObject *)&Assembler_type);
+	return m;
+}
---
+++ libasmase/python/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup, Extension
+
+module = Extension(
+    name='asmase',
+    sources=['asmase_module.c', 'asmase_assembler.c'],
+    include_dirs=['../include'],
+    library_dirs=['..'],
+    libraries=['asmase'],
+)
+
+setup(name='libasmase',
+      ext_modules=[module])
