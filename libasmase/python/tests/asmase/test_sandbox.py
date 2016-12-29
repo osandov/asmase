ac5d02c94a0c9d5d0e39089489058fd15a4c170c
Omar Sandoval <osandov@osandov.com>
2016-12-28 23:57:57 -0800

    Test ASMASE_SANDBOX_ALL

--- libasmase/python/tests/asmase/test_sandbox.py
+++ libasmase/python/tests/asmase/test_sandbox.py
@@ -1,4 +1,5 @@
 import asmase
+from contextlib import contextmanager
 import errno
 import os
 import signal
@@ -7,33 +8,51 @@ import unittest
 from tests.asmase import AsmaseTestCase
 
 
+@contextmanager
+def _instance(*args, **kwds):
+    instance = asmase.Instance(*args, **kwds)
+    try:
+        yield instance
+    finally:
+        instance.destroy()
+
+
 class TestSandbox(AsmaseTestCase):
     def setUp(self):
         self.assembler = asmase.Assembler()
         self.instance = None
 
+    def _test_fds(self, flags):
+        with _instance(flags) as instance:
+            pid = instance.get_pid()
+            self.assertEqual(os.listdir('/proc/{}/fd'.format(pid)), [])
+
     def test_fds(self):
-        self.instance = asmase.Instance(asmase.ASMASE_SANDBOX_FDS)
-        pid = self.instance.get_pid()
-        self.assertEqual(os.listdir('/proc/{}/fd'.format(pid)), [])
+        self._test_fds(asmase.ASMASE_SANDBOX_FDS)
+        self._test_fds(asmase.ASMASE_SANDBOX_ALL)
+
+    def _test_syscalls(self, flags):
+        with _instance(flags) as instance:
+            wstatus = instance.execute_code(self.assembler.assemble_code('nop'))
+            self.assertTrue(os.WIFSTOPPED(wstatus))
+            self.assertEqual(os.WSTOPSIG(wstatus), signal.SIGTRAP)
+
+            code = self.assembler.assemble_code(
+                'movq $231, %rax\n'
+                'movq $0, %rbx\n'
+                'syscall'
+            )
+            wstatus = instance.execute_code(code)
+            self.assertTrue(os.WIFSTOPPED(wstatus))
+            self.assertEqual(os.WSTOPSIG(wstatus), signal.SIGSYS)
 
     def test_syscalls(self):
-        self.instance = asmase.Instance(asmase.ASMASE_SANDBOX_SYSCALLS)
+        self._test_syscalls(asmase.ASMASE_SANDBOX_SYSCALLS)
+        self._test_syscalls(asmase.ASMASE_SANDBOX_ALL)
 
-        wstatus = self.execute_code('nop')
-        self.assertTrue(os.WIFSTOPPED(wstatus))
-        self.assertEqual(os.WSTOPSIG(wstatus), signal.SIGTRAP)
-
-        wstatus = self.execute_code(
-            'movq $231, %rax\n'
-            'movq $0, %rbx\n'
-            'syscall'
-        )
-        self.assertTrue(os.WIFSTOPPED(wstatus))
-        self.assertEqual(os.WSTOPSIG(wstatus), signal.SIGSYS)
-
-    def get_environ(self):
-        with open('/proc/{}/environ'.format(self.instance.get_pid()), 'rb') as f:
+    @staticmethod
+    def get_environ(instance):
+        with open('/proc/{}/environ'.format(instance.get_pid()), 'rb') as f:
             data = f.read()
         if not data:
             return []
@@ -41,29 +60,38 @@ class TestSandbox(AsmaseTestCase):
             data = data[:-1]
         return data.split(b'\0')
 
+    def _test_environ(self, flags):
+        with _instance(flags) as instance:
+            self.assertFalse(self.get_environ(instance))
+
     def test_environ(self):
-        self.instance = asmase.Instance()
-        try:
-            self.assertTrue(self.get_environ())
-        finally:
-            self.instance.destroy()
-            self.instance = None
-        self.instance = asmase.Instance(asmase.ASMASE_SANDBOX_ENVIRON)
-        self.assertFalse(self.get_environ())
+        with _instance() as instance:
+            self.assertTrue(self.get_environ(instance))
+        self._test_environ(asmase.ASMASE_SANDBOX_ENVIRON)
+        self._test_environ(asmase.ASMASE_SANDBOX_ALL)
+
+    def _test_stack(self, flags):
+        with _instance(flags) as instance:
+            code = self.assembler.assemble_code(
+                'subq $0x20000000, %rsp\n'
+                'movq $0, (%rsp)\n'
+            )
+            wstatus = instance.execute_code(code)
+            self.assertTrue(os.WIFSTOPPED(wstatus))
+            self.assertEqual(os.WSTOPSIG(wstatus), signal.SIGSEGV)
 
     def test_stack(self):
-        self.instance = asmase.Instance(asmase.ASMASE_SANDBOX_STACK)
+        self._test_stack(asmase.ASMASE_SANDBOX_STACK)
+        self._test_stack(asmase.ASMASE_SANDBOX_ALL)
 
-        wstatus = self.execute_code(
-            'subq $0x20000000, %rsp\n'
-            'movq $0, (%rsp)\n'
-        )
-        self.assertTrue(os.WIFSTOPPED(wstatus))
-        self.assertEqual(os.WSTOPSIG(wstatus), signal.SIGSEGV)
+    def _test_cpu(self, flags):
+        with _instance(flags) as instance:
+            prio = os.getpriority(os.PRIO_PROCESS, instance.get_pid())
+            self.assertEqual(prio, 19)
 
     def test_cpu(self):
-        self.instance = asmase.Instance(asmase.ASMASE_SANDBOX_CPU)
-        self.assertEqual(os.getpriority(os.PRIO_PROCESS, self.instance.get_pid()), 19)
+        self._test_cpu(asmase.ASMASE_SANDBOX_CPU)
+        self._test_cpu(asmase.ASMASE_SANDBOX_ALL)
 
     def test_invalid(self):
         with self.assertRaises(OSError) as e:
